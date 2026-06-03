24eef2e30a30561b2f1e1605b053c8d9fe42a1e0
Kyorisan <contact.kyorisan@gmail.com>
2026-06-03 09:49:56 +0200

    Adding source files into new project

---
+++ src/main/java/com/nnk/springboot/repository/BidListRepository.java
@@ -0,0 +1,7 @@
+package com.nnk.springboot.repository;
+
+import com.nnk.springboot.domain.BidList;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BidListRepository extends JpaRepository<BidList,Integer> {
+}
--- src/test/java/com/nnk/springboot/PoseidonApplicationTests.java
+++ src/test/java/com/nnk/springboot/PoseidonApplicationTests.java
@@ -1,4 +1,4 @@
-package com.nnk.springboot.Poseidon;
+package com.nnk.springboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
